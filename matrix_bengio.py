58cae2523c1ca96511cd8c8b88c656a3800a3eda
VMijangos <victor.mijangosc@gmail.com>
2019-05-07 19:19:14 -0500

    Update matrix_bengio.py

--- matrix_bengio.py
+++ matrix_bengio.py
@@ -3,26 +3,34 @@ from nphones import nPhones
 from Neural import Model
 import numpy as np
 
+#abre el archivo
 file = open(argv[1], 'r')
+
+#extrae los nphones, variar este parámetro implica tomar uniphones, triphones u otros
 phones = nPhones(file.read(),nphone_siz=1)
 phones.get_phones()
 
-print(phones.voc)
-
+#Aprende el modelo de Bengio
 model = Model(phones.word_phones, ngramas=2)
 model.train(its=50)
 
+#Tamaño de nphones
 N = len(phones.voc)
+
+#Crea matriz de probabilidades de transición
 P = np.zeros((N,N))
 for w in model.voc:
 #	print(phones.voc[w])
 	P[phones.voc[w]] = model.forward([w])
 
+#Probabilidades marginales a partir de P
 k = P.sum(0)
 mu = k/k.sum(0)
 
+#Crea una matriz con log(p_ij)
 logP = np.log(P)
 
+#Calcula las entropias: normal (H) y normalizada entre 0 y 1 (Hnorm)
 Hnorm = 0.0
 H = 0.0
 for w,i in phones.voc.items():
@@ -36,20 +44,3 @@ for w,i in phones.voc.items():
 
 print('Entropía:', H)
 print('Entropía norm:', Hnorm)
-
-
-'''
-EOS = '<EOS>'
-BOS = '<BOS>'
-
-corpus1 = ['el perro come un hueso', 'un perro come un hueso', 'el nino salta', 'un nino come','el perro come un hueso', 'un perro come un hueso', 'el nino salta', 'un nino come']
-
-cadenas = [[BOS] + cad.split() + [EOS] for cad in corpus1]
-model = Model(cadenas, ngramas=2)
-model.train()
-print(model.voc)
-print(model.forward(['<BOS>'])[model.voc['un']])
-'''
-
-#print(phones.word_phones)
-#print(model.forward(['per']))
